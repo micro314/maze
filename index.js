02f7838a6c6061c06d6c75021c8aecb2447fc7f3
Michael Rosati <rosati.michael@gmail.com>
2022-01-27 11:59:41 -0500

    Add more shapes to complete Matter demo

--- index.js
+++ index.js
@@ -1,13 +1,17 @@
 const { Engine, Render, Runner, World, Bodies, MouseConstraint, Mouse } = Matter;
 
+const width = 800;
+const height = 600;
+
 const engine = Engine.create();
 const { world } = engine;
 const render = Render.create({
     element: document.body,
     engine: engine,
     options: {
-        width: 800,
-        height: 600
+        width: width,
+        height: height,
+        wireframes: false
     }
 });
 Render.run(render);
@@ -19,10 +23,21 @@ World.add(world, MouseConstraint.create(engine, {
 const walls = [
     Bodies.rectangle(400, 0, 800, 40, { isStatic: true }),
     Bodies.rectangle(400, 600, 800, 40, { isStatic: true }),
-    Bodies.rectangle(0, 300, 40, 600, { isStatic: true}),
-    Bodies.rectangle(800, 300, 40, 600, { isStatic: true})
+    Bodies.rectangle(0, 300, 40, 600, { isStatic: true }),
+    Bodies.rectangle(800, 300, 40, 600, { isStatic: true })
 ];
 World.add(world, walls);
 
-const rect = Bodies.rectangle(200, 200, 50, 50)
-World.add(world, rect);
+for (let i = 0; i < 20; i++) {
+    let shape;
+    if (Math.random() > 0.5) {
+        shape = Bodies.rectangle(Math.random() * width, Math.random() * height, 50, 50);
+    } else {
+        shape = Bodies.circle(Math.random() * width, Math.random() * height, 35, {
+            render: {
+                fillStyle: 'red'
+            }
+        });
+    }
+    World.add(world, shape);
+}
